ad0ef02786f1e36f90e337b0a3d138d27d3aa7cf
EllsaG <seninlsa@gmail.com>
2024-06-26 22:03:35 +0400

    Added files

---
+++ src/main/java/com/project/psdcompensationdeviceservice/PsdCompensationDeviceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.psdcompensationdeviceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsdCompensationDeviceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsdCompensationDeviceServiceApplication.class, args);
+	}
+
+}
